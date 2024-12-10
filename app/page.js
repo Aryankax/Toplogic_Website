cf1ed6745803d435d2f74364ee3bfff38f8fd686
Aryan Kacker <kackeraryan@gmail.com>
2024-12-10 22:41:19 +0530

    made more responsive

--- app/page.js
+++ app/page.js
@@ -12,6 +12,7 @@ import mobile from "../images/mobile.png";
 import digital from "../images/digital_transformation.png";
 import IndustryDiv from "@/components/industryDiv";
 import automotive from "../images/automotive.jpg";
+import { faL } from "@fortawesome/free-solid-svg-icons";
 
 // Register ScrollTrigger plugin
 gsap.registerPlugin(ScrollTrigger);
@@ -49,8 +50,8 @@ const Home = () => {
                 trigger: "#industries",
                 scroller: "body",
                 markers: false,
-                start: "top 15%",
-                end: "top -550%",
+                start: "top 20%",
+                end: "top -560%",
                 scrub: 2,
                 pin: true
             }
@@ -85,13 +86,13 @@ const Home = () => {
                     className="Hero-section-content text-white relative w-screen h-screen z-20"
                     id="Hero-Content"
                 >
-                    <p className="font-Poppins font-bold text-green-500 absolute md:left-20 md:top-44 md:text-[100px] uppercase transition-colors duration-300 hover:text-green-300 left-5 top-48 text-[40px]">
+                    <p className="font-Poppins font-bold text-green-500 absolute md:left-20 md:top-44 md:text-[100px] uppercase transition-colors duration-300 hover:text-green-300 top-44 text-[40px] text-center left-3">
                         Revolutionizing
                     </p>
-                    <p className="font-Poppins font-bold text-green-500 relative md:left-20 md:top-[270px] md:text-[100px] uppercase transition-colors duration-300 hover:text-green-300 left-5 top-60 text-[40px]">
+                    <p className="font-Poppins font-bold text-green-500 relative md:left-20 md:top-[270px] md:text-[100px] uppercase transition-colors duration-300 hover:text-green-300 left-3 top-56 text-[40px]">
                         It Solutions.
                     </p>
-                    <div className="md:w-[800px] h-full top-[290px] md:top-[390px] left-5 md:left-16 md:p-2 text-justify flex items-center justify-center mr-10 ml-5 mt-16 md:ml-[80px] md:mt-2">
+                    <div className="md:w-[800px] h-full md:top-[250px] md:p-1 text-justify md:ml-[80px] md:mt-2 flex mt-60 ml-3 mr-2 relative">
                         <p className="font-Poppins text-[18px]">
                             Toplogic Softlabs is at the forefront of innovation, delivering cutting-edge IT solutions that are transforming industries. With a focus on excellence and sustainability, we provide tailor-made technology services designed to streamline operations, enhance efficiency, and drive growth. We are committed to revolutionizing the digital landscape, ensuring our clients stay ahead in the fast-paced world of technology.
                         </p>
@@ -154,12 +155,12 @@ const Home = () => {
                 </section>
 
                 {/* New Section */}
-                <section className="relative top-20 left-14">
-                    <h1 className="text-white uppercase font-Poppins font-extrabold text-5xl">Elevate Your</h1>
-                    <h1 className="text-white uppercase font-Poppins font-extrabold text-5xl">Business With</h1>
-                    <h1 className="text-white uppercase font-Poppins font-extrabold text-5xl">Digital <span className="text-green-800">innovation</span>.</h1>
-                    <div className="w-[500px] relative top-8">
-                        <p className="text-gray-300 text-lg">At <span className="text-white font-bold">Toplogic Softlabs</span>, we excel in delivering world-class <span className="text-white font-bold">digital transformation consulting services</span> tailored to bridge the gap between <span className="text-white font-bold">business strategies and innovation</span>. Our team of experts specializes in reimagining business processes by integrating <span className="text-white font-bold">cutting-edge digital technologies</span> that empower enterprises to embrace modern digital ecosystems.
+                <section className="relative md:top-20 md:left-14 top-10 left-10 w-screen md:h-[1390px]">
+                    <h1 className="text-white uppercase font-Poppins font-extrabold md:text-5xl text-4xl">Elevate Your</h1>
+                    <h1 className="text-white uppercase font-Poppins font-extrabold md:text-5xl text-4xl">Business With</h1>
+                    <h1 className="text-white uppercase font-Poppins font-extrabold md:text-5xl text-4xl">Digital <span className="text-green-800">innovation</span>.</h1>
+                    <div className="md:w-[500px] relative md:top-8 w-[300px] text-justify top-2">
+                        <p className="text-gray-300 md:text-lg">At <span className="text-white font-bold">Toplogic Softlabs</span>, we excel in delivering world-class <span className="text-white font-bold">digital transformation consulting services</span> tailored to bridge the gap between <span className="text-white font-bold">business strategies and innovation</span>. Our team of experts specializes in reimagining business processes by integrating <span className="text-white font-bold">cutting-edge digital technologies</span> that empower enterprises to embrace modern digital ecosystems.
                         <span className="text-white font-bold"> Unlike one-size-fits-all solutions</span>, <span className="text-white font-bold">our approach focuses on developing a customized digital transformation framework</span>. We meticulously analyze your existing operating models and align them with <span className="font-bold text-white">industry-leading practices</span> to ensure seamless adaptation. Our consultants help businesses of all sizes—from <span className="font-bold text-white">startups to established industry leaders</span>—strategize and invest in open, <span className="font-bold text-white">scalable technologies</span> that <span className="text-white font-bold">elevate customer experiences</span>.
                         By <span className="text-white font-bold">leveraging automation, data-driven insights, and streamlined operations</span>, Toplogic Softlabs ensures your business stays ahead in the digital age. Unlock the potential of digital innovation with solutions designed to enhance <span className="text-white font-bold">efficiency</span>, <span className="text-white font-bold">agility</span>, and <span className="text-white font-bold">competitive advantage</span>.
                         </p>
@@ -168,29 +169,29 @@ const Home = () => {
                         <div className="">
                                 <Image src={cloud} width={60} height={undefined} className="h-auto relative left-32 top-1" alt="Cloud"></Image>
                                 <p className="font-Poppins font-bold text-xl">Cloud technology</p>
-                                <p className="text-center text-base text-gray-300 p-2 rounded-xl relative top-2">We streamline your journey to the cloud, offering seamless migration and comprehensive management services. From complex infrastructure transitions to optimizing existing cloud assets, we ensure a smooth and efficient process. Our agile, scalable, and secure cloud environments empower your business to innovate faster, reduce costs, and drive growth.</p>
+                                <p className="text-base text-gray-300 p-2 rounded-xl relative top-2 text-justify">We streamline your journey to the cloud, offering seamless migration and comprehensive management services. From complex infrastructure transitions to optimizing existing cloud assets, we ensure a smooth and efficient process. Our agile, scalable, and secure cloud environments empower your business to innovate faster, reduce costs, and drive growth.</p>
                         </div>
                         <div className="">
                                 <Image src={ai} width={50} height={undefined} className="h-auto relative left-32 top-2" alt="AI"></Image>
                                 <p className="font-Poppins font-bold text-xl relative top-3">Artificial Intelligence</p>
-                                <p className="text-center text-base text-gray-300 p-2 rounded-xl relative left-4 top-4">Optimize Your Operations with Intelligent Automation. Our experts implement RPA solutions to automate repetitive tasks and complex workflows. By integrating seamlessly with your ERP systems, we boost efficiency, reduce errors, and drive cost savings. This allows your team to focus on strategic initiatives and deliver exceptional results.</p>
+                                <p className="text-base text-gray-300 p-2 rounded-xl relative left-4 top-4 text-justify">Optimize Your Operations with Intelligent Automation. Our experts implement RPA solutions to automate repetitive tasks and complex workflows. By integrating seamlessly with your ERP systems, we boost efficiency, reduce errors, and drive cost savings. This allows your team to focus on strategic initiatives and deliver exceptional results.</p>
                         </div>
                         <div className="">
-                                <Image src={digital} width={40} height={undefined} className="h-auto relative left-32 top-1" alt="Digital transformation"></Image>
-                                <p className="font-Poppins font-bold text-xl relative top-3">Digital Transformation Consulting</p>
-                                <p className="text-center text-base text-gray-300 p-2 rounded-xl relative top-2">Our seasoned experts dive deep into your specific challenges, providing tailored strategies to drive sustainable growth. Through in-depth evaluations and actionable insights, we empower you to make informed decisions and achieve measurable results.</p>
+                                <Image src={digital} width={50} height={undefined} className="h-[50px] relative md:left-32 md:top-3" alt="Digital transformation"></Image>
+                                <p className="font-Poppins font-bold text-xl relative md:top-5 text-center">Digital Transformation Consulting</p>
+                                <p className="text-base text-gray-300 p-2 relative md:top-5 text-justify">Our seasoned experts dive deep into your specific challenges, providing tailored strategies to drive sustainable growth. Through in-depth evaluations and actionable insights, we empower you to make informed decisions and achieve measurable results.</p>
                         </div>
                         <div className="">
-                                <Image src={mobile} width={50} height={undefined} className="h-auto relative left-32 top-1" alt="Mobile"></Image>
-                                <p className="font-Poppins font-bold text-xl relative top-3">Enterprise Mobility & App Developmenty</p>
-                                <p className="text-center text-base text-gray-300 p-2 rounded-xl relative top-2 left-4">Our expert team develops cutting-edge mobile apps that enhance customer engagement and drive business growth. From cross-platform to custom solutions, we leverage the latest technologies to deliver seamless user experiences and optimize your mobile presence.</p>
+                                <Image src={mobile} width={50} height={undefined} className="md:h-50 relative left-32 top-1" alt="Mobile"></Image>
+                                <p className="font-Poppins font-bold text-xl relative md:top-5">Enterprise Mobility & App Developmenty</p>
+                                <p className="text-base text-gray-300 p-2 relative md:top-5 md:left-4 text-justify">Our expert team develops cutting-edge mobile apps that enhance customer engagement and drive business growth. From cross-platform to custom solutions, we leverage the latest technologies to deliver seamless user experiences and optimize your mobile presence.</p>
                         </div>
                     </div>
                 </section>
                 <section className="Industries">
-                    <div className="border-2 border-gray-200 relative -top-[460px] h-[540px] bg-gray-200" id="industries">
-                        <h1 className="text-center font-Poppins font-extrabold text-[70px] relative top-1 uppercase">Industries</h1>
-                        <div className="relative max-w-full px-10 top-6" id="industryScrollContainer" ref={industryRef}>
+                    <div className="relative -top-[400px] h-[540px] bg-gray-200" id="industries">
+                        <h1 className="text-center font-Poppins font-extrabold md:text-[70px] relative md:top-1 uppercase text-[40px] top-2">Industries</h1>
+                        <div className="relative max-w-full px-10 top-12 md:top-4" id="industryScrollContainer" ref={industryRef}>
             <div className="flex gap-12 pb-6" id="horizontal-container">
                 <IndustryDiv Name={"Automotive"} imageSrc={automotive}></IndustryDiv>
                 <IndustryDiv Name={"Real Estate"}></IndustryDiv>
@@ -203,9 +204,9 @@ const Home = () => {
                 <IndustryDiv Name={"Fintech"}></IndustryDiv>
                 <IndustryDiv Name={"Travel & Tourism"}></IndustryDiv>
                 <IndustryDiv Name={"Entertainment"}></IndustryDiv>
-            </div>
-        </div>
-                    </div>
+                </div>
+                </div>
+                </div>
                 </section>
             </div>
         </>
