823f6cf7095481b3562d3738c4ce81b127ecf35a
Aryan Kacker <kackeraryan@gmail.com>
2024-12-13 01:21:33 +0530

    made website much more responsive

--- app/page.js
+++ app/page.js
@@ -11,7 +11,6 @@ import ai from "../images/ai_2.png";
 import mobile from "../images/mobile.png";
 import digital from "../images/digital_transformation.png";
 import IndustryDiv from "@/components/industryDiv";
-import servicesCard from "@/components/servicesCard";
 import ServicesCard from "@/components/servicesCard";
 import webDev from "../images/web-dev.png";
 import appDev from "../images/app-dev.png";
@@ -93,14 +92,15 @@ const Home = () => {
 
     return (
         <>
-            <div className="relative bg-black min-h-screen overflow-x-hidden overflow-y-hidden sm:overflow-x-scroll">
+            <div className="relative bg-black min-h-screen overflow-x-hidden   sm:overflow-x-scroll w-screen">
                 <div className="fixed top-0 left-0 right-0 z-50">
                     <Navbar />
                 </div>
+                
 
                 {/* Video Background */}
                 <video
-                    className="hidden absolute w-screen h-[720px] md:h-[700px] z-10 md:object-cover md:block"
+                    className="hidden absolute w-screen h-[720px] md:h-[640px] z-10 md:object-cover md:block"
                     src="/Toplogic_Video.mp4"
                     autoPlay
                     loop
@@ -112,19 +112,21 @@ const Home = () => {
 
                 {/* Hero Section Content */}
                 <section
-                    className="Hero-section-content text-white relative w-screen h-screen z-20"
+                    className="Hero-section-content text-white relative w-screen h-screen md:h-[640px] z-20"
                     id="Hero-Content"
                 >
-                    <p className="font-Poppins font-bold text-green-500 absolute md:left-20 md:top-44 md:text-[100px] uppercase transition-colors duration-300 hover:text-green-300 top-44 text-[40px] text-center left-3">
+                    <div className="md:relative md:flex md:flex-col md:left-10 md:h-screen md:w-screen md:justify-center">
+                    <p className="font-Poppins font-bold text-green-500 absolute md:top-44 md:text-[100px] uppercase transition-colors duration-300 hover:text-green-300 top-44 text-[40px] ml-3">
                         Revolutionizing
                     </p>
-                    <p className="font-Poppins font-bold text-green-500 relative md:left-20 md:top-[270px] md:text-[100px] uppercase transition-colors duration-300 hover:text-green-300 left-3 top-56 text-[40px]">
+                    <p className="font-Poppins font-bold text-green-500 relative md:top-[270px] md:text-[100px] uppercase transition-colors duration-300 hover:text-green-300 top-56 text-[40px] ml-3">
                         It Solutions.
                     </p>
-                    <div className="md:w-[800px] h-full md:top-[250px] md:p-2 text-justify md:ml-[80px] md:mt-2 flex mt-60 ml-3 mr-6 relative">
+                    <div className="md:w-[800px] h-full md:top-[250px] md:p-2 text-justify md:mt-2 flex mt-60 ml-3 mr-6 relative">
                         <p className="font-Poppins text-[18px]">
                             Toplogic Softlabs is at the forefront of innovation, delivering cutting-edge IT solutions that are transforming industries. With a focus on excellence and sustainability, we provide tailor-made technology services designed to streamline operations, enhance efficiency, and drive growth. We are committed to revolutionizing the digital landscape, ensuring our clients stay ahead in the fast-paced world of technology.
                         </p>
+                        </div>
                     </div>
                 </section>
 
@@ -183,38 +185,39 @@ const Home = () => {
                     </div>
                 </section>
 
-                {/* New Section */}
-                <section className="relative md:top-20 md:left-14 top-10 left-10 w-screen md:h-[1390px]">
-                    <h1 className="text-white uppercase font-Poppins font-extrabold md:text-5xl text-4xl">Elevate Your</h1>
+                <section className="relative md:top-20 md:left-14 top-10 left-10 w-screen md:h-[1390px] h-[2800px]">
+                    <h1 className="text-white uppercase font-Poppins font-extrabold md:text-5xl text-5xl">Elevate Your</h1>
                     <h1 className="text-white uppercase font-Poppins font-extrabold md:text-5xl text-4xl">Business With</h1>
-                    <h1 className="text-white uppercase font-Poppins font-extrabold md:text-5xl text-4xl">Digital <span className="text-green-800">innovation</span>.</h1>
+                    <h1 className="text-white uppercase font-Poppins font-extrabold md:text-5xl text-3xl">Digital <span className="text-green-800">innovation</span>.</h1>
+                    <div className="flex flex-col items-center relative -left-10 top-5">
                     <div className="md:w-[500px] relative md:top-8 w-[300px] text-justify top-2">
                         <p className="text-gray-300 md:text-lg">At <span className="text-white font-bold">Toplogic Softlabs</span>, we excel in delivering world-class <span className="text-white font-bold">digital transformation consulting services</span> tailored to bridge the gap between <span className="text-white font-bold">business strategies and innovation</span>. Our team of experts specializes in reimagining business processes by integrating <span className="text-white font-bold">cutting-edge digital technologies</span> that empower enterprises to embrace modern digital ecosystems.
                         <span className="text-white font-bold"> Unlike one-size-fits-all solutions</span>, <span className="text-white font-bold">our approach focuses on developing a customized digital transformation framework</span>. We meticulously analyze your existing operating models and align them with <span className="font-bold text-white">industry-leading practices</span> to ensure seamless adaptation. Our consultants help businesses of all sizes—from <span className="font-bold text-white">startups to established industry leaders</span>—strategize and invest in open, <span className="font-bold text-white">scalable technologies</span> that <span className="text-white font-bold">elevate customer experiences</span>.
                         By <span className="text-white font-bold">leveraging automation, data-driven insights, and streamlined operations</span>, Toplogic Softlabs ensures your business stays ahead in the digital age. Unlock the potential of digital innovation with solutions designed to enhance <span className="text-white font-bold">efficiency</span>, <span className="text-white font-bold">agility</span>, and <span className="text-white font-bold">competitive advantage</span>.
                         </p>
                     </div>
-                    <div className="relative text-white w-[650px] -top-[550px] left-[520px] h-[700px] grid grid-cols-2 grid-rows-2 text-center">
+                    <div className="relative text-white md:w-[650px] md:-top-[550px] md:left-[520px] md:h-[700px] md:grid md:grid-cols-2 md:grid-rows-2 md:text-center top-[30px]">
                         <div className="">
-                                <Image src={cloud} width={60} height={undefined} className="h-auto relative left-32 top-1" alt="Cloud"></Image>
-                                <p className="font-Poppins font-bold text-xl">Cloud technology</p>
-                                <p className="text-base text-gray-300 p-2 rounded-xl relative top-2 text-justify">We streamline your journey to the cloud, offering seamless migration and comprehensive management services. From complex infrastructure transitions to optimizing existing cloud assets, we ensure a smooth and efficient process. Our agile, scalable, and secure cloud environments empower your business to innovate faster, reduce costs, and drive growth.</p>
+                                <Image src={cloud} width={60} height={undefined} className="md:h-auto relative md:left-32 md:top-1 left-32" alt="Cloud"></Image>
+                                <p className="font-Poppins font-bold text-xl relative left-16 mt-3 mb-3">Cloud technology</p>
+                                <p className="text-base text-gray-300 md:p-2 rounded-xl relative md:top-2 md:text-justify w-[300px] h-[200px] text-justify">We streamline your journey to the cloud, offering seamless migration and comprehensive management services. From complex infrastructure transitions to optimizing existing cloud assets, we ensure a smooth and efficient process. Our agile, scalable, and secure cloud environments empower your business to innovate faster, reduce costs, and drive growth.</p>
                         </div>
-                        <div className="">
-                                <Image src={ai} width={50} height={undefined} className="h-auto relative left-32 top-2" alt="AI"></Image>
-                                <p className="font-Poppins font-bold text-xl relative top-3">Artificial Intelligence</p>
-                                <p className="text-base text-gray-300 p-2 rounded-xl relative left-4 top-4 text-justify">Optimize Your Operations with Intelligent Automation. Our experts implement RPA solutions to automate repetitive tasks and complex workflows. By integrating seamlessly with your ERP systems, we boost efficiency, reduce errors, and drive cost savings. This allows your team to focus on strategic initiatives and deliver exceptional results.</p>
+                        <div className="mt-20">
+                                <Image src={ai} width={50} height={undefined} className="md:h-auto relative md:left-32 md:top-1 left-32" alt="AI"></Image>
+                                <p className="font-Poppins font-bold text-xl relative left-12 mt-3 mb-3">Artificial Intelligence</p>
+                                <p className="text-base text-gray-300 md:p-2 rounded-xl relative md:top-2 md:text-justify w-[300px] h-[200px] text-justify">Optimize Your Operations with Intelligent Automation. Our experts implement RPA solutions to automate repetitive tasks and complex workflows. By integrating seamlessly with your ERP systems, we boost efficiency, reduce errors, and drive cost savings. This allows your team to focus on strategic initiatives and deliver exceptional results.</p>
                         </div>
-                        <div className="">
-                                <Image src={digital} width={50} height={undefined} className="h-[50px] relative md:left-32 md:top-3" alt="Digital transformation"></Image>
-                                <p className="font-Poppins font-bold text-xl relative md:top-5 text-center">Digital Transformation Consulting</p>
-                                <p className="text-base text-gray-300 p-2 relative md:top-5 text-justify">Our seasoned experts dive deep into your specific challenges, providing tailored strategies to drive sustainable growth. Through in-depth evaluations and actionable insights, we empower you to make informed decisions and achieve measurable results.</p>
+                        <div className="mt-20">
+                                <Image src={digital} width={50} height={undefined} className="h-[50px] relative md:left-32 md:top-3 left-32" alt="Digital transformation"></Image>
+                                <p className="font-Poppins font-bold md:text-xl relative md:top-5 text-center text-xl mt-3 mb-3 w-[300px]">Digital Transformation Consulting</p>
+                                <p className="text-base text-gray-300 md:p-2 rounded-xl relative md:top-2 md:text-justify w-[300px] h-[200px] text-justify ">Our seasoned experts dive deep into your specific challenges, providing tailored strategies to drive sustainable growth. Through in-depth evaluations and actionable insights, we empower you to make informed decisions and achieve measurable results.</p>
                         </div>
-                        <div className="">
-                                <Image src={mobile} width={50} height={undefined} className="md:h-50 relative left-32 top-1" alt="Mobile"></Image>
-                                <p className="font-Poppins font-bold text-xl relative md:top-5">Enterprise Mobility & App Developmenty</p>
-                                <p className="text-base text-gray-300 p-2 relative md:top-5 md:left-4 text-justify">Our expert team develops cutting-edge mobile apps that enhance customer engagement and drive business growth. From cross-platform to custom solutions, we leverage the latest technologies to deliver seamless user experiences and optimize your mobile presence.</p>
+                        <div className="mt-10">
+                                <Image src={mobile} width={50} height={undefined} className="md:h-50 relative left-32 top-1 m" alt="Mobile"></Image>
+                                <p className="font-Poppins font-bold md:text-xl relative md:top-5 text-center text-xl mt-3 mb-3 w-[300px]">Enterprise Mobility & App Developmenty</p>
+                                <p className="text-base text-gray-300 md:p-2 rounded-xl relative md:top-2 md:text-justify w-[300px] h-[200px] text-justify ">Our expert team develops cutting-edge mobile apps that enhance customer engagement and drive business growth. From cross-platform to custom solutions, we leverage the latest technologies to deliver seamless user experiences and optimize your mobile presence.</p>
                         </div>
+                    </div>
                     </div>
                 </section>
                 <section className="Industries">
@@ -241,7 +244,8 @@ const Home = () => {
                 <section className="services">
                     <h1 className="text-[40px] font-lato text-white text-center font-extrabold  uppercase relative -top-[380px] md:text-[70px]">Services</h1>
                     <h1 className="text-green-700 text-lg font-lato font-bold relative -top-[380px]  text-center md:text-[25px]">Where Ideas Transform into <span className="text-white">Digital Excellence</span>.</h1>
-                    <div className="relative -top-[350px] ml-2 md:flex md:flex-row md:flex-wrap md:left-28">
+                    <div className="flex justify-center items-center min-h-screen">
+                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 relative -top-[370px] gap-2">
                         <ServicesCard ServiceName={"Web Development"} ImgSrc={webDev} TitleSize={"xl"}></ServicesCard>
                         <ServicesCard ServiceName={"App Development"} ImgSrc={appDev} TitleSize={"xl"}></ServicesCard>
                         <ServicesCard ServiceName={"Software Development"} ImgSrc={software} TitleSize={"xl"}></ServicesCard>
@@ -249,9 +253,10 @@ const Home = () => {
                         <ServicesCard ServiceName={"Devops"} ImgSrc={devops} TitleSize={"xl"}></ServicesCard>
                         <ServicesCard ServiceName={"SEO"} ImgSrc={seo} TitleSize={"xl"}></ServicesCard>
                     </div>
+                    </div>
                 </section>
                 <section className="Latest Technologies">
-      <div className="w-screen bg-slate-200 h-[1250px] relative -top-80 md:h-[850px]">
+      <div className="w-screen bg-slate-200 h-[1250px] relative -top-80 md:h-screen">
         <div className="relative top-8 md:top-2">
           <h1 className="text-[35px] font-Poppins text-black text-center font-bold uppercase relative top-3 md:text-[60px]">
             Using advanced
@@ -262,10 +267,11 @@ const Home = () => {
           <h1 className="text-[35px] font-Poppins text-black text-center font-bold uppercase relative top-3 md:text-[60px]">
             to maximise value.
           </h1>
-          <p className="ml-5 mr-5 text-center relative top-10 font-lato font-normal md:ml-10 md:top-5 md:font-bold md:text-lg">
+          <p className="ml-5 mr-5 text-center relative top-10 font-lato font-normal md:ml-10 md:top-8 md:font-bold md:text-xl">
             We deliver the highest level of customer service by deploying innovative and collaborative project management systems to build the most professional, robust, and highly scalable web & mobile solutions with the highest quality standards.
           </p>
-          <div className="relative bg-green-800 w-[350px] h-[400px] left-[20px] top-16 overflow-y-auto md:ml-[50px] md:w-[600px] md:rounded-xl">
+          <div className="flex flex-col items-center justify-center md:flex md:flex-row md:items-center">
+          <div className="relative bg-green-800 w-[80%] h-[400px] top-16 overflow-y-auto md:w-[40%] md:rounded-xl rounded-t-xl">
             <ol className="relative ml-5 mt-5 text-2xl flex flex-col gap-14 text-white font-bold mb-5">
               <li className="border-b pb-4" onClick={() => toggleContent('ai')}>
                 1. Artificial Intelligence
@@ -284,7 +290,7 @@ const Home = () => {
               </li>
             </ol>
           </div>
-          <div className="tech-content relative mt-16 ml-5 bg-gray-300 w-[350px] h-[420px] rounded-xl md:relative md:-top-[400px] md:left-[600px] md:z-20 md:h-[400px] md:w-[600px]">
+          <div className="tech-content relative mt-16 bg-gray-300 w-[80%] h-[440px] md:relative md:w-[40%] md:z-20 md:h-[400px] rounded-b-xl md:rounded-xl md:top-8">
             {selectedTech === 'ai' && (
               <>
                 <p className="font-Poppins font-bold text-black text-4xl relative top-10 left-5">
@@ -339,6 +345,7 @@ const Home = () => {
                 </p>
               </>
             )}
+          </div>
           </div>
         </div>
       </div>
